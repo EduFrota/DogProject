109ac4492535ff0d5c07804cecf9552161ac2e53
Eduardo Frota <edu-frota@hotmail.com>
2018-10-31 22:10:10 -0300

    Login quase completo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+
+
+
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -43,6 +46,10 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
+
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
