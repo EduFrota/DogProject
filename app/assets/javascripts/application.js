27b80627bd6037fc30ea5d47b0a01be0a938aabc
EduFrota <edu-frota@hotmail.com>
2019-05-01 22:38:28 -0300

    Fix Deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 
 
-//= require jquery
+//= require jquery/dist/jquery
 //= require rails-ujs
 //= require materialize-css/dist/js/materialize
 //= require activestorage
